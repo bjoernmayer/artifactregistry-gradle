d4908fd89cdee84bda4a2de24adc776bbd64e8b3
Björn Mayer <mayer.bjoern@web.de>
2024-04-19 15:18:48 +0200

    v0.1

--- artifactregistry-gradle-plugin/build.gradle.kts
+++ artifactregistry-gradle-plugin/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("jvm") version "1.9.23"
 
     id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
+    id("com.gradle.plugin-publish") version "1.2.1"
 }
 
 kotlin {
@@ -23,9 +24,19 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
 }
 
+group = "com.github.bjoernmayer"
+version = "0.1"
+
 gradlePlugin {
+    website = "https://github.com/bjoernmayer/artifactregistry-gradle"
+    vcsUrl = "https://github.com/bjoernmayer/artifactregistry-gradle"
+
     val artifactregistryGradlePlugin by plugins.creating {
         id = "com.github.bjoernmayer.artifactregistryGradlePlugin"
         implementationClass = "com.github.bjoernmayer.artifactregistrygradle.ArtifactRegistryGradlePlugin"
+
+        displayName = "ArtifactRegistry Gradle Plugin"
+        description = "Automatically handle authentication with Maven repositories hosted on Artifact Registry."
+        tags = listOf("maven", "artifact", "repositories", "googleCloud")
     }
 }
