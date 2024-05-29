d1d4a15e0a6fea0c5fafbde023984ddce049ab3f
Björn Mayer <mayer.bjoern@web.de>
2024-05-29 13:07:54 +0200

    prepare release

--- artifactregistry-gradle-plugin/build.gradle.kts
+++ artifactregistry-gradle-plugin/build.gradle.kts
@@ -40,7 +40,7 @@ configurations.all {
 }
 
 group = "io.github.bjoernmayer"
-version = "0.2.1"
+version = "0.2.2"
 
 gradlePlugin {
     website = "https://github.com/bjoernmayer/artifactregistry-gradle"
